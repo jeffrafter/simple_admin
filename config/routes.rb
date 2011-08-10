b693ace89ee2a486eb76807d7fe22eb869fdf6d3
Nick Plante <nap@zerosum.org>
2011-08-10 02:04:42 -0400

    add a default route for /admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SimpleAdmin::Engine.routes.draw do
+  root :to => "admin#index", :interface => SimpleAdmin::registered.first.try(:collection)
   SimpleAdmin::registered.each do |interface|
     resources interface.collection,
               :controller => :admin,
